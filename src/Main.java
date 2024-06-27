f643a9ce1ee5b2e4430f1fa733cc3d4599cd46cd
Fabian Maximilian Spielberg <fabian.m.spielberg@gmail.com>
2024-06-27 12:56:01 +0200

    bug fixes for everyone

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ import logging.Logger;
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
         Logger root = new Logger("root");
-        root.setLevel(Level.ALL);
+        root.setLevel(Level.INFO);
         root.addHandler(new logging.ConsoleLogger());
-        new Logger("Network.Connection").setLevel(Level.INFO);
+//        new Logger("Network.Connection").setLevel(Level.INFO);
         Server server = new Server(8080, "127.0.0.1", 3306, "informatik_game", "zugf5bVMC7&hVa43HQglV?3W0CIPYGFbR09!nT#", "informatik_game");
         server.start();
     }
